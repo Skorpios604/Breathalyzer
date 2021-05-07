1650e25f9ebe7f6913e8b14b6f1f25ada2f884c2
Skorpios <70910395+Skorpios604@users.noreply.github.com>
2021-05-06 20:34:49 -0700

    Add files via upload

--- Breathalyzer.c
+++ Breathalyzer.c
@@ -1,93 +1,177 @@
+#include <LiquidCrystal.h>
 
-// Declare the libraries that are necessary for the LCD screen
+// LiquidCrystal I2C - Version: Latest 
+#include <LiquidCrystal_I2C.h>
 
-#include 
-#include 
+//library
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-// Declare a warmup time of 20 seconds for the MQ-3 sensor, which is necessary for good functioning of the sensor
-
+//warmup
 int TIME_UNTIL_WARMUP = 20;
 unsigned long time;
 
-// Declare a time for each measurement; the time for which the user will have to blow on the sensor for the measurement
-
+//measurement
 int TIME_UNTIL_MEASURE = 5;
 unsigned long measurement_start;
 
-// Declare the pin of the MQ-3 sensor
-
+//sensor readings
 int analogPin = A0;
 int val = 0;
 
-// Declare the pin of the piezo buzzer
-
+//buzzer
 const int buzzerPin = 7;
 
-// Pin of the push button:
+//button
+const int buttonPin = 2; 
 
-const int buttonPin = 2;
+//modes
+bool measurement_mode;
+bool measurement_done;
 
-// Initialise the I2C LCD instance:
+//LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2); //set the LCD address to 0x27 for a 16 chars and 2 line display
 
-LiquidCrystal_I2C lcd(0x27,20,4);
-
-// In the setup(), initialise the LCD screen, and activate the backlight
-
-lcd.init(); 
-lcd.backlight();
-
-// In the loop() part of the sketch, we check if the button was pressed. If so, reset the whole measurement process
-
-int button_state = digitalRead(buttonPin);
-
-if (button_state && !measurement_mode) {
-  lcd.clear();
-  measurement_mode = true;
-  measurement_start = millis()/1000; 
-  measurement_done = false;
+void setup()
+{
+  //serial
+  Serial.begin(9600);
+  
+  //Init LCD
+  lcd.init();
+  lcd.backlight();
+  
 }
 
-// Keep track of the time with a counter:
-
-time = millis()/1000;
-
-// If still in the warmup process, we wait, and print this status on the LCD screen
-
-if(time<=time_until_warmup) {=""  ="" int="" progress_time="map(time," 0,="" time_until_warmup,="" 100);="" printwarming(progress_time);="" }<="" pre="">
-
-// Once warmup is done, wait until user presses the button to start the measurement process
-
-else
+void loop()
 {
-  if (measurement_mode == false && !measurement_done) {
-    
-    // Instruction 
-    printPress(); 
+  //read button state
+  int button_state = digitalRead(buttonPin);
+  
+  if(button_state && !measurement_mode)
+  {
+    lcd.clear();
+    measurement_mode = true;
+    measurement_start = millis()/1000;
+    measurement_done = false;
   }
-
-  if (measurement_mode && !measurement_done) {
-
-    // Instruction 
-    printMeasure();
-
-    // Sound
-    tone(buzzerPin, 1000);
-
-    // Read alcohol level
-    val = readAlcohol();
+  //wait
+  delay(100);
+  
+  //get time
+  time = millis()/1000;
+  
+  //warmup
+  if(time<=TIME_UNTIL_WARMUP)
+  {
+    int progress_time = map(time, 0, TIME_UNTIL_WARMUP, 0, 100);
+    printWarming(progress_time);
   }
   
-  if (measurement_mode && !measurement_done && ((time - measurement_start)> TIME_UNTIL_MEASURE)){
-
-    noTone(buzzerPin);
-    measurement_mode = false;
-    measurement_done = true;
-    lcd.clear();
+  else
+  {
+    if (measurement_mode == false && !measurement_done)
+    {
+      //instruction
+      printPress();
+    }
+    if (measurement_mode && !measurement_done)
+    {
+      //instruction
+      printMeasure();
+      
+      //sound
+      tone(buzzerPin, 1000);
+      
+      //read alcohol level
+      val = readAlcohol();
+    
+    }
+    
+    if (measurement_mode && !measurement_done && ((time - measurement_start) > TIME_UNTIL_MEASURE))
+    {
+      noTone(buzzerPin);
+      measurement_mode = false;
+      measurement_done = true;
+      lcd.clear();
+    }
+    
+    if (measurement_done)
+    {
+      printAlcohol(val);
+      printAlcoholLevel(val);
+    } 
   }
-
-  if(measurement_done) {
-
-    printAlcohol(val);
-    printAlcoholLevel(val); 
+  
   }
-}
+  
+  void printWarming (int progress)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Warming up: ");
+    lcd.print(progress);
+    lcd.print("%");
+  }
+  
+  void printPress()
+  {
+    lcd.setCursor(0,0);
+    lcd.print ("Press to start...");
+  }
+  
+  void printMeasure()
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Breathe until the ");
+    lcd.setCursor(0,1);
+    lcd.print("sound stops...");
+  }
+  
+  void printAlcohol(int value)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Sensor reading: ");
+    lcd.print(val);
+  }
+  
+  void printAlcoholLevel (int value)
+  {
+    lcd.setCursor(0,1);
+    if(value<200)
+    {
+      lcd.print("You are sober.");
+    }
+    if(value>=200 && value<280)
+    {
+      lcd.print("You had a beer.");
+    }
+    if(value>=280 && value<350)
+    {
+      lcd.print("Two or more beers.");
+    }
+    if(value>=350 && value <450)
+    {
+      lcd.print("Too much...");
+    }
+    if(value>450)
+    {
+      lcd.print("You are drunk!");
+    }
+  }
+  
+  int readAlcohol()
+  {
+    //number measurements
+    int nb_measurements = 5;
+    int measurements;
+    
+    //measure
+    for(int i=0; i<nb_measurements; i++)
+    {
+      measurements = measurements + analogRead(analogPin);
+    }
+    measurements = measurements/nb_measurements;
+  
+    return measurements;
+  }
+  
